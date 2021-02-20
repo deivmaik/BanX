dc4c7e93cdc84a681876d28eafa52a0829f63336
deivmaik <deivmaik@users.noreply.github.com>
2021-02-20 04:27:24 -0400

    Revert "Added js functionality to balance display"
    This reverts commit 635a1e73d70c4b74e56ce3d339c1374dab809497.

--- ebanX/script.js
+++ ebanX/script.js
@@ -2,42 +2,34 @@
 
 // Data
 const account1 = {
-    owner: 'Guest',
+    owner: 'Michael J',
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
-    owner: 'Henderson',
+    owner: 'Henderson P',
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
 };
 
 const account3 = {
-    owner: 'Bruno',
+    owner: 'Bruno V',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
 };
 
 const account4 = {
-    owner: 'Svend',
+    owner: 'Svend P',
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
 };
 
-const account5 = {
-    owner: 'Sandra',
-    movements: [200, 10000, 7000, -500, -900],
-    interestRate: 1,
-    pin: 5555,
-};
-
-
-const accounts = [account1, account2, account3, account4, account5,];
+const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -64,32 +56,6 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-
-
-// Changing the content of the Balance section
-const displayMovements = function (movements) {
-
-    containerMovements.innerHTML = ' '
-    // This line deletes the actual content before editing the DOM 
-    movements.forEach(function (mov, i) {
-        const type = mov > 0 ? 'deposit' : 'withdrawal';
-        // If the movements are bigger than zero then is deposit, else is withdrawal
-        const html = `
-        <div class="movements__row">
-                <div class="movements__type movements__type--${type}">${i + 1}${' ' + type}</div>
-                <div class="movements__value">${mov}</div>
-            </div>
-        `;
-        // This const changes the info inside the html 
-        containerMovements.insertAdjacentHTML('afterbegin', html)
-        // afterbegin makes the balance start from last to first
-    });
-}
-
-displayMovements(account1.movements);
-// To display the movements 
-
-
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
