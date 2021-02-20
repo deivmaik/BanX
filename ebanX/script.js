ef2ae24268e74693bd8f67b645d3b7cef59f0a3c
deivmaik <deivmaik@users.noreply.github.com>
2021-02-20 03:33:18 -0400

    Finished responsive design

--- ebanX/script.js
+++ ebanX/script.js
@@ -2,28 +2,28 @@
 
 // Data
 const account1 = {
-    owner: 'Michael J',
+    owner: 'Guest',
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
-    owner: 'Henderson P',
+    owner: 'Henderson',
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
 };
 
 const account3 = {
-    owner: 'Bruno V',
+    owner: 'Bruno',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
 };
 
 const account4 = {
-    owner: 'Svend P',
+    owner: 'Svend',
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
