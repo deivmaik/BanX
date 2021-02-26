a95125fccbc6f5a9f1e079fe576df22ae20809e2
deivmaik <deivmaik@users.noreply.github.com>
2021-02-26 16:55:24 -0400

    js done

--- app/js/script.js
+++ app/js/script.js
@@ -25,3 +25,37 @@ hamburger.addEventListener('click', function () {
         })
     }
 })
+
+
+// Testimonials
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.testimonials__slide');
+
+let index = 0;
+display(index);
+function display(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide() {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
